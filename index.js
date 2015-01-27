d3059b7d01d5e27bf2ca11141f5177beb596178b
jreinert <bucket@jreinert.com>
2015-01-28 00:40:55 +0100

    Added Swarm to exports of index.js
    Need this for overriding getSwarm()

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Client = require('./client')
 var Server = require('./server')
+var Swarm = require('./lib/swarm')
 
 module.exports = Client
 module.exports.Client = Client
 module.exports.Server = Server
+module.exports.Swarm = Swarm
